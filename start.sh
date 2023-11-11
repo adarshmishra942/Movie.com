104f9196905fb71412a164d43b2c8b7b1905b254
Adarsh Devendra Mishra <147018728+adarshmishra942@users.noreply.github.com>
2023-11-11 11:35:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone /Movie-Club-New /Movie-Club-New
+  git clone /Movie.com /Movie.com
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Movie-Club-New
+  git clone $UPSTREAM_REPO /Movie.com
 fi
-cd /Movie-Club-New
+cd /Movie.com
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
